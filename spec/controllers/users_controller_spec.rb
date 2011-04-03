a1f3230065168a8a145760ce6e6b44fbf6b32cf1
rubytut <t1m_prv@o2.pl>
2011-04-03 20:46:50 +0200

    + user controler show action RTEST

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -5,6 +5,24 @@ describe UsersController do
   # line since it tests the view along with the action.
   render_views
 
+  describe "GET 'show'" do
+    
+    before(:each) do
+      @user = Factory(:user)
+    end
+
+    it "should be successful" do
+      get :show, :id => @user
+      response.should be_success
+    end
+
+    it "should find the right user" do
+      get :show, :id => @user
+      assigns(:user).should == @user
+    end
+  end # describe GET show
+
+
   describe "GET 'new'" do
     it "should be successful" do
       get 'new'

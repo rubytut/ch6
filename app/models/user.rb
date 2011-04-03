c12d2ca35a36f8429f4536191e92926663aac094
rubytut <t1m_prv@o2.pl>
2011-04-03 15:29:57 +0200

    add case_sensitive email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,7 +23,7 @@ class User < ActiveRecord::Base
   email_regexp = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, :presence   => true,
                     :format     => { :with =>email_regexp },
-		    :uniqueness => true
+		    :uniqueness => { :case_sensitive => false }
 
 
 

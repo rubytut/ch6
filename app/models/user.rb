b8a6859e24bcb3bd741357105c240ac1422bee9c
rubytut <t1m_prv@o2.pl>
2011-04-03 19:33:56 +0200

    + usr password encryption methods (GREEN)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
+require 'digest'
 # == Schema Information
 # Schema version: 20110403092218
 #
@@ -46,15 +47,27 @@ class User < ActiveRecord::Base
   def has_password?(submitted_password)
     # Compare encrypted_password with the encrypted version of
     # submitted_password.
+    encrypted_password == encrypt(submitted_password)
   end
 
   private
     def encrypt_password
+      # 'new_record?' returns true if the object
+      # has not yet been saved to the database.
+      self.salt = make_salt if new_record?
       self.encrypted_password = encrypt(password)
     end
 
     def encrypt(string)
-      string # Only temporary implementation !
+      secure_hash("#{salt}--#{string}")
+    end
+
+    def make_salt
+      secure_hash("#{Time.now.utc}--#{password}")
+    end
+
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
     end
 
 end

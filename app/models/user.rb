a5e68980f6cf81862b3f458d0dfdd6cfcdd34965
rubytut <t1m_prv@o2.pl>
2011-04-03 13:17:48 +0200

    add name validator and test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,21 @@
+# == Schema Information
+# Schema version: 20110403092218
+#
+# Table name: users
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class User < ActiveRecord::Base
+
+  # Which attributes can be modified by outside users
+  # (such as users submiting requests with web browsers)
+  attr_accessible :name, :email
+
+  # validate the attributes before accepting them
+  validates :name, :presence => true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,24 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  # run the block before each example
+  before(:each) do
+    @attr = { :name => "Example User", :email => "user@example.com" }
+  end
+
+  it "should create a new instance given valid attributes" do
+    # The 'create!' method raises ActiveRecord::RecordInvalid exception
+    # if the creation fails. As long as the attributes are valid, it
+    # won't raise any exceptions, and the test will pass.
+    User.create!(@attr)
+  end
+
+  # failing test for user without a name
+  it "should require a name" do
+    no_name_user = User.new(@attr.merge(:name => ""))
+
+    # same as: no_name_user.valid?.should_not == true
+    no_name_user.should_not be_valid
+  end
 end

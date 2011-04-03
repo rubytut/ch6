6ee6008a369c7cae48ce5bdb14ef564ef54bbf0c
rubytut <t1m_prv@o2.pl>
2011-04-03 20:42:40 +0200

    + test Factory for user controller

--- Gemfile
+++ Gemfile
@@ -13,9 +13,10 @@ group :development do
 end
 
 group :test do
-	gem 'rspec', '2.5.0'
-	gem 'webrat', '0.7.1'
-	gem 'spork', '0.9.0.rc3'
+	gem 'rspec',              '2.5.0'
+	gem 'webrat',             '0.7.1'
+	gem 'spork',              '0.9.0.rc3'
+	gem 'factory_girl_rails', '1.0'
 end
 
 # Use unicorn as the web server
---
+++ spec/factories.rb
@@ -0,0 +1,17 @@
+# Tests fo the Users controller will need instances of User model objects, 
+# preferably with pre-defined values. For example the Users controller show 
+# action needs an instance of the User class, so the tests for this action will 
+# require that we create an @user variable somehow. 
+# We'll accomplish this goal with a user 'factory', which is a convenient way 
+# to define a user object and insert it into our test database.
+#
+# By putting the factories.rb file in the spec/ directory, we arrange for RSpec 
+# to load our factories automatically whenever the test run.
+#
+# By using the symbol ':user', we get Factory Girl to simulate the User model.
+Factory.define :user do |user|
+  user.name                  "Michael Hartl"
+  user.email                 "mhartl@example.com"
+  user.password              "foobar"
+  user.password_confirmation "foobar"
+end

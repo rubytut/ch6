2ce8df8660a2fcd4d85191277497d4162b1befca
rubytut <t1m_prv@o2.pl>
2011-04-03 18:27:49 +0200

    add user password RTEST

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,12 @@ describe User do
 
   # run the block before each example
   before(:each) do
-    @attr = { :name => "Example User", :email => "user@example.com" }
+    @attr = { 
+      :name => "Example User", 
+      :email => "user@example.com",
+      :password => "foobar",
+      :password_confirmation => "foobar"
+    }
   end
 
   it "should create a new instance given valid attributes" do
@@ -70,6 +75,27 @@ describe User do
     user_with_duplicate_email = User.new(@attr)
     user_with_duplicate_email.should_not be_valid
   end
+
+  describe "password validations" do
+
+    it "should require a password" do
+      User.new(@attr.merge(:password => "", :password_confirmation => ""))
+        should_not be_valid
+    end
+
+    it "should reject short passwords" do
+      short = "a" * 5
+      has = @attr.merge(:password => short, :password_confirmation => short)
+      User.new(hash).should_not be_valid
+    end
+
+    it "should reject long passwords" do
+      long = "a" * 41
+      hash = @attr.merge(:password => long, :password_confirmation => long)
+      User.new(hash).should_not be_valid
+    end
+
+  end # describe
 
 end
 

16b9426622ad3c2b7e8b1107c54bc37915bf206f
rubytut <t1m_prv@o2.pl>
2011-04-03 15:53:10 +0200

    add user show action controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,15 @@
 class UsersController < ApplicationController
+
   def new
     @title = "Sign up"
+  end
+
+  def show
+    # 'params' is a standart Rails object to retrive the user id.
+    # When we make the appropriate request to the Users controller, 
+    # params[:id] will be the user id 1, so the effect is the same
+    # as the find command: 'User.find(1)'.
+    @user = User.find(params[:id])
   end
 
 end

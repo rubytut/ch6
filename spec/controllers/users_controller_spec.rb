a49aef68387cd275a9a05df8b4e3c6162a6406a1
rubytut <t1m_prv@o2.pl>
2011-04-04 19:24:23 +0200

    + flash success RTEST (R)

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -99,6 +99,12 @@ describe UsersController do
         post :create, :user => @attr
         response.should redirect_to(user_path(assigns(:user)))
       end
+
+      it "should have a welcome message" do
+        post :create, :user => @attr
+        flash[:success].should =~ /welcome to the sample app/i
+      end
+
     end # describe success
 
   end # describe POST create

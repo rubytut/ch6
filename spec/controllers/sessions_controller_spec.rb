f26d3e2246ac515c29181d19791a557cbf1a438e
rubytut <t1m_prv@o2.pl>
2011-04-08 22:09:04 +0200

    + signin success rtests (r)

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -40,5 +40,23 @@ describe SessionsController do
       end
 
     end # describe invalid signin
+
+    describe "with valid email and password" do
+
+      before(:each) do
+        @user = Factory(:user)
+        @attr = { :email => @user.email, :password => @user.password }
+      end
+
+      it "should sign the user in" do
+        post :create, :session => @attr
+        # Fill in with tests for a signed-in user
+      end
+
+      it "should redirect to the user show page" do
+        post :create, :session => @attr
+        response.should redirect_to(user_path(@user))
+      end
+    end # describe 'with valid email and password'
   end # describe Post create
 end

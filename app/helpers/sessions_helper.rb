d987f7f6baa083e14717d60403222333204033a8
rubytut <t1m_prv@o2.pl>
2011-04-08 22:30:13 +0200

    + current_user= assignmet function

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,4 +4,8 @@ module SessionsHelper
     cookies.permanent.signed[:remember_token] = [user.id, user.salt]
     self.current_user = user
   end
+
+  def current_user=(user)
+    @current_user = user
+  end
 end

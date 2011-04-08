a66c09729f45d2f92df33330284312de5c284211
rubytut <t1m_prv@o2.pl>
2011-04-08 23:35:08 +0200

    sign in, signup user (g)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,7 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     if @user.save
       # handle a succesful save
+      sign_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else

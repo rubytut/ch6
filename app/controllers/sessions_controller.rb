b1c281864514cc0492490c0dd566a87f635574da
rubytut <t1m_prv@o2.pl>
2011-04-08 21:58:47 +0200

    + authenticate fail code

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,15 @@ class SessionsController < ApplicationController
   end
 
   def create
-    render 'new'
+    user = User.authenticate(params[:session][:email],
+                             params[:session][:password])
+    if user.nil?
+      flash.now[:error] = "Invalid email/password combination."
+      @title = "Sign in"
+      render 'new'
+    else
+      # Sign the user in and redirect to the user's show page
+    end
   end
 
 end

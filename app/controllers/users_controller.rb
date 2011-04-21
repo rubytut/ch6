bb443873311669e16ab0469c03caa87ac3b380f0
rubytut <t1m_prv@o2.pl>
2011-04-21 17:20:50 +0200

    + before_filter for authentication (g)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  before_filter :authenticate, :only => [:edit, :update]
+  # By default, before filters apply to every action in a controller,
+  # so here we restrict the filter to act only on the
+  # :edit and :update actions by passing the :only options hash.
 
   def new
     @user = User.new

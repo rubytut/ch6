1a2258241e53caaa2be1eef918903245ef21d65c
rubytut <t1m_prv@o2.pl>
2011-04-21 21:54:38 +0200

    use paginate in users index controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,8 @@ class UsersController < ApplicationController
 
   def index
     @title = "All users"
-    @users = User.all
+    ##@users = User.all
+    @users = User.paginate(:page => params[:page])
   end
 
   def show

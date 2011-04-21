6f45e02c0b2a960576725c6c826be928243e3aba
rubytut <t1m_prv@o2.pl>
2011-04-21 23:00:58 +0200

    implement user destroy action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
-  before_filter :authenticate, :only => [:edit, :update, :index]
+  before_filter :authenticate, :only => [:edit, :update, :index, :destroy]
   # By default, before filters apply to every action in a controller,
   # so here we restrict the filter to act only on the
   # :edit and :update actions by passing the :only options hash.
 
   before_filter :correct_user, :only => [:edit, :update]
+  before_filter :admin_user,   :only => :destroy
 
   def new
     @user = User.new
@@ -42,6 +43,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_path
+  end
+
 
   def edit
     @user = User.find(params[:id])
@@ -70,4 +77,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     redirect_to(root_path) unless current_user?(@user)
   end
+
+  def admin_user
+    redirect_to(root_path) unless current_user.admin?
+  end
+
+
 end

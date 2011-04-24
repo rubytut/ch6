a50665f894acffd88b15227d0aaf5ce9e1e77114
rubytut <t1m_prv@o2.pl>
2011-04-24 13:00:32 +0200

    micropost test

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe Micropost do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @attr = {
+      :content => "value for content",
+      :user_id => 1
+    }
+  end
+
+  it "should create a new instance given valid attributes" do
+    Micropost.create!(@attr)
+  end
+
 end

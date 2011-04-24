63f51a5b0dae6ae3fc1531442dc596a6ede4f2ef
rubytut <t1m_prv@o2.pl>
2011-04-24 15:45:03 +0200

    micropost create rtests

--- spec/controllers/microposts_controller_spec.rb
+++ spec/controllers/microposts_controller_spec.rb
@@ -3,6 +3,58 @@ require 'spec_helper'
 describe MicropostsController do
   render_views
 
+  describe "POST 'create'" do
+
+    before(:each) do
+      @user = test_sign_in(Factory(:user))
+    end
+
+    describe "failure" do
+
+      before(:each) do
+        @attr = { :content => "")
+      end
+
+      it "should not create a micropost" do
+        lambda do
+          post :create, :micropost => @attr
+        end.should_not change(Micropost, :count)
+      end
+
+      it "should render the home page" do
+        post :create, :micropost => @attr
+        response.should render_template('pages/home')
+      end
+    end # describe failure
+
+    describe "success" do
+
+      before(:each) do
+        @attr = { :content => "Lorem ipsum" }
+      end
+
+      it "should create a micropost" do
+        lambda do
+          post :create, :micropost => @attr
+        end.should change(Micropost, :count).by(1)
+      end
+
+      it "should redirect to the home page" do
+        post :create, :micropost => @attr
+        response.should redirect_to(root_path)
+      end
+
+      it "should have a flas message" do
+        post :create, :micropost => @attr
+        flash[:success].should =~ /micropost created/i
+      end
+    end # success
+
+  end # POST create
+
+
+
+
   describe "access control" do
 
     it "should deny access to 'create'" do

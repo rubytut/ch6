d0f61b638100562c551fcfacfc0a2eb268726a20
rubytut <t1m_prv@o2.pl>
2011-04-24 12:57:09 +0200

    make :content attribute editable through web

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,2 +1,9 @@
 class Micropost < ActiveRecord::Base
+  # Failing to define accessble attributes means that anyone
+  # could change any aspect of a micropost object simpy by 
+  # using command-line client to issue malicious request.
+  # In the case of the Micropost model, there is only one 
+  # attribute that needs to be editable through the web, namely,
+  # to content attribute.
+  attr_accessible :content
 end

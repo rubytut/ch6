d9cbfc971aad3246e82affb074fb41aede7fbf02
rubytut <t1m_prv@o2.pl>
2011-04-30 16:00:48 +0200

    test user-relationships belongs_to association (r)

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -13,4 +13,28 @@ describe Relationship do
     @relationship.save!
   end
 
+  describe "follow methods" do
+
+    before(:each) do
+      @relationship.save
+    end
+
+    it "should have a follower attribute" do
+      @relationship.should respond_to(:follower)
+    end
+
+    it "should have the right follower" do
+      @relationship.follower.should == @follower
+    end
+
+    it "should have a followed attribute" do
+      @relationship.should respond_to(:followed)
+    end
+
+    it "should have the right followed user" do
+      @relationship.followed.should == @followed
+    end
+
+  end # follow methods
+
 end

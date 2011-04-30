dff140a19c06cb92798711071270125a948b97b6
rubytut <t1m_prv@o2.pl>
2011-04-30 20:35:41 +0200

    test for the Relationships controller responses to Ajax requests (r)

--- spec/controllers/relationships_controller_spec.rb
+++ spec/controllers/relationships_controller_spec.rb
@@ -28,6 +28,14 @@ describe RelationshipsController do
         response.should be_redirect
       end.should change(Relationship, :count).by(1)
     end
+
+    it "should create a relationship using Ajax" do
+      lambda do
+        xhr :post, :create, :relationship => { :followed_id => @followed }
+        response.should be_success
+      end.should change(Relationship, :count).by(1)
+    end
+
   end # POST create
 
   describe "DELETE 'destroy'" do
@@ -45,6 +53,14 @@ describe RelationshipsController do
         response.should be_redirect
       end.should change(Relationship, :count).by(-1)
     end
+
+    it "should destroy a relationship using Ajax" do
+      lambda do
+        xhr :delete, :destroy, :id => @relationship
+        response.should be_success
+      end.should change(Relationship, :count).by(-1)
+    end
+
   end # DELETE destroy
 
 end

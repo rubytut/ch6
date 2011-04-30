c3c6ebf01312dd24659f48c579fcb34c028d1935
rubytut <t1m_prv@o2.pl>
2011-04-30 15:47:32 +0200

    relationships tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -216,6 +216,18 @@ describe User do
 
   end # describe micropost associations
 
+  describe "relationships" do
+
+    before(:each) do
+      @user = User.create!(@attr)
+      @followed = Factory(:user)
+    end
+
+    it "should have a relationships method" do
+      @user.should respond_to(:relationships)
+    end
+  end # relation ships
+
 
 end
 

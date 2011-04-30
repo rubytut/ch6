b4fbfa3ac048328dab0a2d0518306074634f21f2
rubytut <t1m_prv@o2.pl>
2011-04-30 10:15:30 +0200

    add an empty @feed_items instance variable to the create action

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -8,6 +8,7 @@ class MicropostsController < ApplicationController
       flash[:success] = "Micropost created!"
       redirect_to root_path
     else
+      @feed_items = []
       render 'pages/home'
     end
   end

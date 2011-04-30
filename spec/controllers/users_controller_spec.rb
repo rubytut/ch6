8f6b33cb483b17b235b2b7a1cd1768aa7d96c59b
rubytut <t1m_prv@o2.pl>
2011-04-30 18:59:08 +0200

    test for the following and followers actions (r)

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -383,4 +383,72 @@ describe UsersController do
       end
     end
   end # describe authentication of edit/update
+
+  describe "follow pages" do
+    describe "when no signed in" do
+
+      it "should protect 'following'" do
+        get :following, :id => 1
+        response.should redirect_to(singin_path)
+      end
+
+      it "should protect 'followers'" do
+        get :followers, :id => 1
+        response.should redirect_to(signin_path)
+      end
+
+    end # when not signed in
+
+    describe "when signed in" do
+
+      before(:each) do
+        @user = test_sign_in(Factory(:user))
+        @other_user = Factory(:user, :email => Factory.next(:email))
+        @user.follow!(@other_user)
+      end
+
+      it "should show user following" do
+        get :following, :id => @user
+        response.should have_selector("a", :href => user_path(@other_user),
+                                      :content => @other_user.name)
+      end
+
+      it "should show user followers" do
+        get :followers, :id => @other_user
+        response.should have_selector("a", :href => user_path(@user),
+                                      :content => @user.name)
+      end
+    end # when signed in
+
+  end # follow pages
+  
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

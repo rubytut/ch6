60e829962b2f5b1f6da1eee06ebd4c2f11cda6be
rubytut <t1m_prv@o2.pl>
2011-04-30 10:42:14 +0200

    implement Microposts controller destroy action

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,6 +1,6 @@
 class MicropostsController < ApplicationController
-#  before_filter :authenticate, :only => [:create, :destroy]
-  before_filter :authenticate
+  before_filter :authenticate, :only => [:create, :destroy]
+  before_filter :authorized_user, :only => :destroy
 
   def create
     @micropost = current_user.microposts.build(params[:micropost])
@@ -14,6 +14,15 @@ class MicropostsController < ApplicationController
   end
 
   def destroy
+    @micropost.destroy
+    redirect_back_or root_path
+  end
+
+  private
+
+  def authorized_user
+    @micropost = Micropost.find(params[:id])
+    redirect_to root_path unless current_user?(@micropost.user)
   end
 
 end
